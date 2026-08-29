1342c036dc6fbbc15bf2583c50561214be44b077
felipe.magalhaes <felipemlopes@proton.me>
2026-08-28 23:00:36 -0400

    update CreateHabit

--- src/components/create-habit/CreateHabit.tsx
+++ src/components/create-habit/CreateHabit.tsx
@@ -2,7 +2,8 @@ const CreateHabit = () => {
   return (
     <div className="bg-white text-black">
       <div className="h-96 w-64">
-        <div className="">Create a new habit</div>
+        <div className="">Create a new habit 88</div>
+        <input type="text" title="test" value={2} />
       </div>
     </div>
   );
