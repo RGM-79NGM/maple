e54dd6629d12cbb0b42134ad6303b4e7e953b66e
felipe.magalhaes <felipemlopes@proton.me>
2026-08-31 02:44:15 -0400

    chore: update eslint and prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import importX from 'eslint-plugin-import-x';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
@@ -7,20 +9,33 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+
+    plugins: {
+      'import-x': importX,
     },
+
+    settings: {
+      'import-x/resolver-next': [createTypeScriptImportResolver()],
+    },
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'import-x/no-unresolved': 'error',
     },
   },
 ]);
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-css-order"],
+  cssDeclarationSorterOrder: "smacss",
+  endOfLine: "auto",
+  singleQuote: true
+};
