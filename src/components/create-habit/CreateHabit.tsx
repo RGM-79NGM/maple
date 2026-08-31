01ff72934a7c6e65158306df8fd1e1e0e1286c0d
felipe.magalhaes <felipemlopes@proton.me>
2026-08-31 03:41:49 -0400

    chore: test adding single quotes

--- src/components/create-habit/CreateHabit.tsx
+++ src/components/create-habit/CreateHabit.tsx
@@ -1,7 +1,7 @@
 const CreateHabit = () => {
   return (
     <div className="bg-white text-black">
-      <div className="h-96 w-64">
+      <div className='h-96 w-64'>
         <div className="">Create a new habit 88</div>
         <input type="text" title="test" value={2} />
       </div>
