43b0d141b5e6dac7196593749f9c412beabc4e8d
Felipe Magalhães <yvy.05@proton.me>
2026-09-23 22:53:03 -0400

    chore: rename MainPage component declaration and export. User new logo in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className="relative flex items-center justify-end px-10 py-4">
       <div className="absolute top-0 left-10 flex flex-col leading-0.5">
-        <img src="/maple-purple.png" alt="" className="size-24" />
-        <span className="text-center">Maple</span>
+        <img src="/logo.png" alt="" className="size-24" />
+        <span className="text-center font-semibold">Maple</span>
       </div>
 
       <Cog8ToothIcon
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,7 +4,7 @@ import Modal from '../components/Modal';
 import CreateHabit from '../components/create-habit/CreateHabit';
 import Header from '../components/Header';
 
-const Core = () => {
+const MainPage = () => {
   // const [entries, setEntries] = useState([]);
   // const [average, setAverage] = useState();
   // const [total, setTotal] = useState();
@@ -17,26 +17,25 @@ const Core = () => {
   // ];
 
   return (
-      <div className="flex flex-col justify-center">
-        <Header />
+    <div className="flex flex-col justify-center">
+      <Header />
 
-        <h3 className="flex w-full items-baseline justify-center pt-5 text-center text-4xl font-bold">
-          <span className="w-36 text-center">Habits</span>
-          <span
-            // onClick={createStickyWindow}
-            onClick={() => setOpenCreateHabitModal(true)}
-            className="material-icons flex cursor-pointer items-center justify-center rounded-sm bg-gray-800 p-0.5 text-[20px]! transition duration-200 hover:bg-gray-900"
-          >
-            add
-          </span>
-        </h3>
-        <div className="mt-7 flex h-full w-full justify-center">
-          <HeatMap />
-        </div>
-      {openCreateHabitModal && <Modal content={<CreateHabit />} />}
+      <h3 className="flex w-full items-baseline justify-center pt-5 text-center text-4xl font-bold">
+        <span className="w-36 text-center">Habits</span>
+        <span
+          // onClick={createStickyWindow}
+          onClick={() => setOpenCreateHabitModal(true)}
+          className="material-icons flex cursor-pointer items-center justify-center rounded-sm bg-gray-800 p-0.5 text-[20px]! transition duration-200 hover:bg-gray-900"
+        >
+          add
+        </span>
+      </h3>
+      <div className="mt-7 flex h-full w-full justify-center">
+        <HeatMap />
       </div>
-
+      {openCreateHabitModal && <Modal content={<CreateHabit />} />}
+    </div>
   );
 };
 
-export default Core;
+export default MainPage;
